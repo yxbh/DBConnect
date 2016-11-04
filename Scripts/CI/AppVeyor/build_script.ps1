91b23bb2d93a2da309f6d5cb44c585a28891750a
Benjamin Huang <y.x.ben.h+github@gmail.com>
2016-11-04 20:21:58 +1100

    fix appveyor build script won't exit with error on compile failure

--- Scripts/CI/AppVeyor/build_script.ps1
+++ Scripts/CI/AppVeyor/build_script.ps1
@@ -4,8 +4,14 @@ cd src
 
 # build DEBUG build.
 qmake DBConnect.pro "CONFIG+=debug"
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 mingw32-make
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # build RELEASE build.
 qmake DBConnect.pro
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 mingw32-make
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
